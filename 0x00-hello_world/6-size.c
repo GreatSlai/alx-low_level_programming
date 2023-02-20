1c1cdec7f262ad786b61f5cca9a6f65c27e37b48
GreatSlai <databoardcomm@gmail.com>
2023-02-19 22:17:43 -0800

    Sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of long int: %zu bytes\n", sizeof(longintType));
 	printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType);
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
 
 	return (0);
 
